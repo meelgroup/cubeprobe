b0a6a5a1fed67ee24041f9aa8d1f74a3f0a08275
uddaloksarkar <uddaloksarkar@gmail.com>
2023-07-21 20:18:09 +0530

    pbs scripts add

---
+++ codes/run_verifier.py
@@ -0,0 +1,34 @@
+import sys
+import os
+import tempfile
+
+tempDir = tempfile.gettempdir()
+processrankStr = os.environ.get("OMPI_COMM_WORLD_RANK")
+
+if (processrankStr == None):
+    processrankStr = 0
+processrank = int(processrankStr)
+
+all_file_name = str(tempDir)+'/all_files_'+str(processrank)+'.log'
+cmd = 'find benchmarks -name \*.cnf >' + all_file_name
+os.system(cmd)
+
+f = open(all_file_name,'r')
+lines = f.readlines()
+f.close()
+
+samplerType = 4-(processrank/len(lines))
+#samplerType = 2
+if (samplerType < 1):
+    exit(-1)
+    
+filepos = lines[processrank%len(lines)].strip()
+fileSuffix = filepos.split('/')[-1][:-4]
+
+cmd ='mkdir outDir'
+os.system(cmd)
+
+#cmd = 'ulimit -v 4000000; python Verifier.py --sampler '+str(samplerType)+' --inverted 1 --reverse 0 --seed 0 --exp 1000 '+filepos+'  outDir/sampler_'+str(samplerType)+'_'+fileSuffix+'.out'
+#print(cmd)
+cmd = 'python3 flash.py --sampler '+str(samplerType)+' --seed 420 '+filepos+'  outDir/sampler_'+str(samplerType)+'_'+fileSuffix+'.out'
+os.system(cmd)
